5f2c3a3d331d9c31f298228d17d671eee8817a2b
cladams0203 <cladams0203@gmail.com>
2020-01-24 21:39:53 -0500

    finished projects router crud

--- projectRouter.js
+++ projectRouter.js
@@ -1,3 +1,48 @@
 const router = require('express').Router()
+const db = require('./data/helpers/projectModel')
+
+router.get('/:id', (req,res) => {
+    db.get(req.params.id)
+        .then(item => {
+            res.status(200).json(item)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({message: 'unable to get project'})
+        })
+})
+
+router.post('/', (req,res) => {
+    db.insert(req.body)
+        .then(item => {
+            res.status(201).json(item)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({message: 'unable to add project'})
+        })
+})
+
+router.put('/:id', (req,res) => {
+    db.update(req.params.id, req.body)
+        .then(item => {
+            res.status(201).json(item)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({message: 'unable to edit project'})
+        })
+})
+
+router.delete('/:id', (req,res) => {
+    db.remove(req.params.id)
+        .then(item => {
+            res.status(201).json({message: `removed ${item} records`})
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({message: 'unable to delete project'})
+        })
+})
 
 module.exports = router
